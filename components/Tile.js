48ad83899533ed1ea9347616002d80b893e89900
David Blatcher <david.blatcher@graduate-jobs.com>
2020-10-07 18:38:20 +0100

    extract frame codes to SpriteSheets module

--- components/Tile.js
+++ components/Tile.js
@@ -1,74 +1,24 @@
 import React from 'react'
 import styles from './tile.module.scss'
+import {spriteSheets, SpriteSheet} from '../lib/SpriteSheet.tsx'
 
-
-const trees = {
-    cols: 3,
-    rows: 4,
-    url: '/spriteSheet.png'
-}
-
-function getRightFrame (sprite, adjacents, message) {
-
-    
-    let bitString = ''
-
-    bitString += adjacents.before && adjacents.before.tree ? "1":"0";
-    bitString += adjacents.after && adjacents.after.tree ? "1":"0";
-    bitString += adjacents.above && adjacents.above.tree ? "1":"0";
-    bitString += adjacents.below && adjacents.below.tree? "1":"0";
-
-    switch (bitString) {
-        case "0000": return[sprite, 0,0];
-        case "0010": return[sprite, 1,0];
-        case "0001": return[sprite, 2,0];
-        
-        case "0100": return[sprite, 0,1];
-        case "1000": return[sprite, 1,1];
-        case "1100": return[sprite, 2,1];
-        
-        case "0011": return[sprite, 0,2];
-        case "0101": return[sprite, 1,2];
-        case "1001": return[sprite, 2,2];
-
-        case "0110": return [sprite, 0,3]
-        case "1010": return [sprite, 1,3]
-        case "1111": return[sprite, 2,3];
-        default: 
-        return [sprite, 2,3]
-    }
-
-}
-
-function makeSpriteStyle (sprite, frameX, frameY) {
-    return {
-        backgroundSize:  `${sprite.cols*100}% ${sprite.rows*100}%`,
-        backgroundPosition: `${100*frameX / (sprite.cols -1)}% ${100*frameY / (sprite.rows-1)}%`,
-        backgroundImage: `url(${sprite.url})`
-    } 
-}
 
 export default class Tile extends React.Component {
 
-
     render() {
         const { mapSquare, adjacentSquares, handleClick} = this.props
-
-
-        let spriteStyle = mapSquare.tree
-        ? makeSpriteStyle( ...getRightFrame(trees, adjacentSquares,`${mapSquare.x}, ${mapSquare.y}`) )
-        :{}
-
 
         return (
         <figure className={styles.tile} onClick={handleClick}>
             <p>{mapSquare.terrain.name}</p>
 
             {mapSquare.tree ? (
-                <div className={styles.treeSprite} style={spriteStyle}></div>
+                <div 
+                    className={styles.spriteHolder} 
+                    style={SpriteSheet.getStyle(spriteSheets.trees, adjacentSquares )}
+                ></div>
             ) : (null) }
         </figure>
         )
-
     }
 }
---
+++ lib/SpriteSheet.tsx
@@ -0,0 +1,67 @@
+
+class SpriteSheet {
+    name: String;
+    cols: number;
+    rows: number;
+    getRightFrame: Function;
+    css: object;
+    constructor(name: String, config: Object = {}) {
+        this.name = name;
+        this.cols = config.cols || 1;
+        this.rows = config.rows || 1;
+        this.css = config.css || {};
+        this.getRightFrame = config.getRightFrame || function () { return [0, 0] }
+    }
+
+    static getStyle(sprite: SpriteSheet, adjacentSquares) {
+
+        let frame = sprite.getRightFrame(adjacentSquares)
+    
+    
+        return Object.assign({
+            backgroundSize: `${sprite.cols * 100}% ${sprite.rows * 100}%`,
+            backgroundPosition: `${100 * frame[0] / (sprite.cols - 1)}% ${100 * frame[1] / (sprite.rows - 1)}%`,
+        }, sprite.css)
+    }
+
+}
+
+const trees = new SpriteSheet('trees', {
+    cols: 3,
+    rows: 4,
+    css: {
+        top: '-15%',
+        backgroundImage: 'url(./spritesheet.png)'
+    },
+    getRightFrame(adjacents) {
+        let bitString = ''
+        bitString += adjacents.before && adjacents.before.tree ? "1" : "0";
+        bitString += adjacents.after && adjacents.after.tree ? "1" : "0";
+        bitString += adjacents.above && adjacents.above.tree ? "1" : "0";
+        bitString += adjacents.below && adjacents.below.tree ? "1" : "0";
+
+        switch (bitString) {
+            case "0000": return [0, 0];
+            case "0010": return [1, 0];
+            case "0001": return [2, 0];
+
+            case "0100": return [0, 1];
+            case "1000": return [1, 1];
+            case "1100": return [2, 1];
+
+            case "0011": return [0, 2];
+            case "0101": return [1, 2];
+            case "1001": return [2, 2];
+
+            case "0110": return [0, 3]
+            case "1010": return [1, 3]
+            case "1111": return [2, 3];
+            default: return [2, 3]
+        }
+    }
+})
+
+const spriteSheets = { trees }
+
+
+export { spriteSheets, SpriteSheet }

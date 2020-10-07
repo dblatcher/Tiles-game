130777a979e4f966f96ded18c89db4a4e30d11e7
David Blatcher <david.blatcher@graduate-jobs.com>
2020-10-07 20:48:21 +0100

    Tile.renderSprite

--- components/Tile.js
+++ components/Tile.js
@@ -1,31 +1,28 @@
 import React from 'react'
 import styles from './tile.module.scss'
-import {spriteSheets, SpriteSheet} from '../lib/SpriteSheet.tsx'
+import { spriteSheets, SpriteSheet } from '../lib/SpriteSheet.tsx'
 
 
 export default class Tile extends React.Component {
 
+    renderSprite(spriteSheet) {
+        const { adjacentSquares } = this.props
+        return (
+            <div
+                className={styles.spriteHolder}
+                style={SpriteSheet.getStyle(spriteSheet, adjacentSquares)}
+            ></div>)
+    }
+
     render() {
-        const { mapSquare, adjacentSquares, handleClick} = this.props
+        const { mapSquare, handleClick } = this.props
 
         return (
-        <figure className={styles.tile} onClick={handleClick}>
-            <p>{mapSquare.terrain.name}</p>
-
-            {mapSquare.road ? (
-                <div 
-                    className={styles.spriteHolder} 
-                    style={SpriteSheet.getStyle(spriteSheets.roads, adjacentSquares )}
-                ></div>
-            ) : (null) }
-
-            {mapSquare.tree ? (
-                <div 
-                    className={styles.spriteHolder} 
-                    style={SpriteSheet.getStyle(spriteSheets.trees, adjacentSquares )}
-                ></div>
-            ) : (null) }
-        </figure>
+            <figure className={styles.tile} onClick={handleClick}>
+                <p>{mapSquare.terrain.name}</p>
+                {mapSquare.road ? this.renderSprite(spriteSheets.roads) : (null)}
+                {mapSquare.tree ? this.renderSprite(spriteSheets.trees) : (null)}
+            </figure>
         )
     }
 }

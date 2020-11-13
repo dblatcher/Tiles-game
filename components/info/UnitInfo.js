70da3d7cc8061ec24c839c097020459bfedc43f0
David Blatcher <david.blatcher@graduate-jobs.com>
2020-11-13 21:31:53 +0000

    read content as static props for unit info pages

--- components/info/UnitInfo.js
+++ components/info/UnitInfo.js
@@ -5,13 +5,12 @@ import styles from './info.module.scss'
 
 export default class UnitInfo extends React.Component {
     render() {
-        const { unitType } = this.props
+        const { unitType, content } = this.props
 
         const prerequisite = techDiscoveries[unitType.prerequisite]
 
         return (
             <article className={styles.article}>
-                <h2>{unitType.displayName}</h2>
 
                 <div className={styles.sideBox}>
                     <figure className={styles.figure}>
@@ -28,13 +27,11 @@ export default class UnitInfo extends React.Component {
                         </tbody>
                     </table>
                 </div>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dignissim semper nibh eu tempor. Suspendisse consequat risus velit, sed ultrices mi facilisis eu. Etiam nec risus quam. Vivamus nec lorem leo. Aliquam id lectus in tellus vehicula aliquet. Donec eleifend nunc maximus, porttitor nisi vitae, bibendum nulla. Nullam turpis est, gravida eget nisl sed, ullamcorper accumsan ex. Curabitur vestibulum massa id quam laoreet pharetra. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec eu molestie lacus, et sodales neque.
-                </p>
-                <p>
-                    Morbi pharetra, est ut dapibus feugiat, ex libero dignissim mauris, vel cursus justo lacus ut enim. Nullam a posuere nunc. Integer egestas felis eu laoreet consectetur. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam erat volutpat. Vivamus vehicula sagittis lorem, quis ornare orci maximus sit amet. Nullam imperdiet sit amet nisi sagittis faucibus. Morbi rutrum sit amet urna vel placerat. Fusce iaculis, ipsum ac ornare semper, diam nisi tempor turpis, nec pulvinar orci erat sit amet augue. Mauris faucibus bibendum dolor, eu lacinia diam.
-                </p>
 
+                <h2>{unitType.displayName}</h2>
+                <p>
+                    {content ? content.description : ""}
+                </p>
                 <div className={styles.clear}></div>
             </article>
         )
--- pages/info/unit/[itemName].js
+++ pages/info/unit/[itemName].js
@@ -1,11 +1,16 @@
+import path from 'path'
+import fs from 'fs'
+
 import React from 'react'
 import { useRouter } from 'next/router'
 import Layout from '../../../components/Layout'
+
+
 import UnitInfo from '../../../components/info/UnitInfo'
 
 import { unitTypes } from '../../../lib/game-entities/Unit.tsx'
 
-const UnitInfoPage = () => {
+const UnitInfoPage = ({content}) => {
     const router = useRouter()
     const { itemName } = router.query
     const unitType = unitTypes[itemName]
@@ -13,7 +18,7 @@ const UnitInfoPage = () => {
     return (
         <Layout backLinkText="Back to Index" backLinkUrl="/info">
             {unitType
-                ? <UnitInfo unitType={unitType} />
+                ? <UnitInfo unitType={unitType} content={content} />
                 : null
             }
         </Layout>
@@ -21,3 +26,34 @@ const UnitInfoPage = () => {
 }
 
 export default UnitInfoPage
+
+export async function getStaticProps(context) {
+
+    let content = {}, filenames = []
+    const filename = context.params.itemName + '.json'
+    const contentDirectory = path.join(process.cwd(), 'content/unit')
+
+    try {
+        filenames = fs.readdirSync(contentDirectory)
+    } catch(error) {
+        console.warn (error)
+    } 
+
+    if (filenames.includes(filename)) {
+        const filePath = path.join(contentDirectory, filename)
+        content = JSON.parse(fs.readFileSync(filePath, 'utf8'))
+    }
+
+
+    return {
+        props: {content}, // will be passed to the page component as props
+    }
+}
+
+export async function getStaticPaths() {
+    return {
+        paths: [
+        ],
+        fallback: true // See the "fallback" section below
+    };
+}

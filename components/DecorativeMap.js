e2a6783aea677e10202184f9970bd91cbca35c36
David Blatcher <david.blatcher@graduate-jobs.com>
2020-12-06 12:56:16 +0000

    DecorativeMap can be customised with props

--- components/DecorativeMap.js
+++ components/DecorativeMap.js
@@ -1,6 +1,6 @@
 import TileBoard from './TileBoard'
-import makeGameState from '../lib/makeGameState'
 import styles from './decorativeMap.module.css'
+import { MapSquare } from '../lib/game-entities/MapSquare.tsx';
 
 export default class DecorativeMap extends React.Component {
 
@@ -12,7 +12,10 @@ export default class DecorativeMap extends React.Component {
     }
 
     componentDidMount() {
-        this.setState({ mapGrid: makeGameState.randomWorld().mapGrid })
+        const { width = 20, height = 20, treeChance = .25 } = this.props
+        this.setState({
+            mapGrid: MapSquare.makeRandomGrid(width, height, treeChance, 0)
+        })
     }
 
     render() {

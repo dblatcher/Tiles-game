55b8e7b38fe6f8ed3345180a1b3b33addb6063bf
David Blatcher <david.blatcher@graduate-jobs.com>
2020-12-24 13:14:59 +0000

    add missing properties to GameState

--- lib/game-entities/GameState.ts
+++ lib/game-entities/GameState.ts
@@ -2,13 +2,26 @@ import { Town } from '../game-entities/Town'
 import { Faction } from '../game-entities/Faction';
 import { Unit } from '../game-entities/Unit';
 import { MapSquare } from '../game-entities/MapSquare';
+import { Message } from '../game-entities/Message';
+import { TextQuestion, OptionsQuestion } from './Questions';
 
 class GameState {
     towns: Array<Town>
     factions: Array<Faction>
     units: Array<Unit>
-    selectedUnit: Unit|null
+    selectedUnit: Unit | null
     mapGrid: Array<Array<MapSquare | null> | null>
+    activeFaction: Faction
+    pendingDialogues: Array<Message | TextQuestion | OptionsQuestion>
+    mapZoomLevel: number
+    mapXOffset: number
+    mapShiftInProgress: boolean
+    fallenUnits: Array<Unit>
+    unitPickDialogueChoices: Array<Unit>
+    openTown: Town
+    mainMenuOpen: boolean
+    factionWindowIsOpen: boolean
+    interfaceMode: string
 }
 
 export { GameState }
